6f9d87fb37bd8e2685c7996adfeb4c2b60a679ee
Diego Matias <dm.diego.bh@gmail.com>
2018-03-21 21:56:11 -0300

    added box and side scroller componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SideScrollerComponent } from './side-scroller/side-scroller.component';
+import { BoxComponent } from './box/box.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideScrollerComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/side-scroller/side-scroller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-scroller',
+  templateUrl: './side-scroller.component.html',
+  styleUrls: ['./side-scroller.component.css']
+})
+export class SideScrollerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
